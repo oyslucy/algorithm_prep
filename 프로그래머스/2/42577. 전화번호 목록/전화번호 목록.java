a7195deab326b511c31b278e89d14f19a1413ebd
Yeonseo Oh <84556636+oyslucy@users.noreply.github.com>
2025-08-15 11:40:58 +0900

    [level 2] Title: 전화번호 목록, Time: 335.69 ms, Memory: 99.4 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,17 +1,32 @@
 import java.util.*;
 
 class Solution {
-    public boolean solution(String[] phone_book) {
-        HashSet<String> map = new HashSet<>(Arrays.asList(phone_book));
-
-        for(String p : phone_book){
-            for(int i = 1; i < p.length(); i++){
-                String prefix = p.substring(0, i);
-                if(map.contains(prefix)){
-                    return false;
-                }
+    public boolean solution(String[] phone_book) {  
+        Arrays.sort(phone_book); // 짧은 전화번호부터
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i+1].startsWith(phone_book[i])) {
+                return false;
             }
         }
-        return true;
+        return true; 
     }
-}
+}   
+        
+        
+       
+
+
+        
+//         HashSet<String> map = new HashSet<>(Arrays.asList(phone_book));
+
+//         for(String p : phone_book){
+//             for(int i = 1; i < p.length(); i++){
+//                 String prefix = p.substring(0, i);
+//                 if(map.contains(prefix)){
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+// }
